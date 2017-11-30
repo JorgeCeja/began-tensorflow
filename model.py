ba7d865a22d24fa21b51c4b19ee36af6176ca93f
Jorge Ceja <jorgeceja123@gmail.com>
2017-11-29 20:44:07 -0800

    [refactor] add comments to model

--- model.py
+++ model.py
@@ -9,6 +9,9 @@ class BEGAN(object):
         self.place_holder = place_holder
 
     def model_inputs(self, image_width, image_height, image_channels, z_dim):
+        """
+        Create the model inputs/tensors
+        """
         inputs_real = tf.placeholder(
             tf.float32, (None, image_width, image_height, image_channels), name='input_real')
         inputs_z = tf.placeholder(tf.float32, (None, z_dim), name='input_z')
@@ -27,6 +30,7 @@ class BEGAN(object):
             return math_ops.maximum(alpha * features, features)
 
     def fully_connected(self, x, output_shape):
+        # flatten and dense
         shape = x.get_shape().as_list()
         dim = np.prod(shape[1:])
 
@@ -36,6 +40,9 @@ class BEGAN(object):
         return x
 
     def decoder(self, h, n, img_dim, channel_dim):
+        """
+        Reconstruction network
+        """
         h = tf.layers.dense(h, img_dim * img_dim * n, activation=None)
         h = tf.reshape(h, (-1, img_dim, img_dim, n))
 
@@ -66,6 +73,9 @@ class BEGAN(object):
         return conv4
 
     def encoder(self, images, n, z_dim, channel_dim):
+        """
+        Feature extraction network 
+        """
         conv1 = tf.layers.conv2d(
             images, n, 3, padding="same", activation=self.leaky_relu)
 
@@ -95,6 +105,9 @@ class BEGAN(object):
         return h
 
     def discriminator(self, images, z_dim, channel_dim, reuse=False):
+        """
+        Create the discriminator network: The autoencoder
+        """
         with tf.variable_scope('discriminator', reuse=reuse):
             x = self.encoder(images, 64, z_dim, channel_dim)
             x = self.decoder(x, 64, 64 // 4, channel_dim)
@@ -102,6 +115,9 @@ class BEGAN(object):
             return x
 
     def generator(self, z, channel_dim, is_train=True):
+        """
+        Create the generator network: Only the encoder part
+        """
         reuse = False if is_train else True
         with tf.variable_scope('generator', reuse=reuse):
             x = self.decoder(z, 64, 64 // 4, channel_dim)
@@ -109,11 +125,15 @@ class BEGAN(object):
             return x
 
     def model_loss(self, input_real, input_z, channel_dim, z_dim, k_t):
+        """
+        Get the loss for the discriminator and generator
+        """
         g_model_fake = self.generator(input_z, channel_dim, is_train=True)
         d_model_real = self.discriminator(input_real, z_dim, channel_dim)
         d_model_fake = self.discriminator(
             g_model_fake, z_dim, channel_dim, reuse=True)
 
+        # l1 loss
         d_real = tf.reduce_mean(tf.abs(input_real - d_model_real))
         d_fake = tf.reduce_mean(tf.abs(g_model_fake - d_model_fake))
 
@@ -123,7 +143,10 @@ class BEGAN(object):
         return d_loss, g_loss, d_real, d_fake
 
     def model_opt(self, d_loss, g_loss, learning_rate, beta1, beta2=0.999):
-        # Get weights and bias to update
+        """
+        Get optimization operations
+        """
+        # Get variables
         g_vars = tf.get_collection(
             tf.GraphKeys.GLOBAL_VARIABLES, "generator")
         d_vars = tf.get_collection(
